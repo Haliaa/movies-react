64228137e4ce00d6f8102cb4841746d031e0a567
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-06-06 13:18:54 +0300

    final

--- src/components/MoviePoster/MoviePoster.js
+++ src/components/MoviePoster/MoviePoster.js
@@ -2,11 +2,22 @@ import React from 'react';
 import {urls} from "../../constants";
 import {Rating} from "react-simple-star-rating";
 
-const MoviePoster = ({movie:{original_title, overview, title, vote_average, vote_count, poster_path, release_date}}) => {
+const MoviePoster = ({
+                         movie: {
+                             original_title,
+                             overview,
+                             title,
+                             vote_average,
+                             vote_count,
+                             poster_path,
+                             release_date
+                         }
+                     }) => {
 
     return (
         <div style={{display: 'flex'}}>
-            <div><img style={{width: '600px', margin:'20px'}} src={`${urls.imageOriginal}/${poster_path}`} alt={title}/></div>
+            <div><img style={{width: '600px', margin: '20px'}} src={`${urls.imageOriginal}/${poster_path}`}
+                      alt={title}/></div>
             <div>
                 <div><h1>title: {title}</h1></div>
                 <hr/>
@@ -14,7 +25,7 @@ const MoviePoster = ({movie:{original_title, overview, title, vote_average, vote
                 <div><h4>Overview:</h4> {overview}</div>
                 <div>Release date: {release_date}</div>
                 <div>
-                    <Rating initialValue={vote_average/2} iconsCount={5}/>{vote_average}/10
+                    <Rating initialValue={vote_average / 2} iconsCount={5}/>{vote_average}/10
                 </div>
                 <div>Votes: {vote_count}</div>
             </div>
