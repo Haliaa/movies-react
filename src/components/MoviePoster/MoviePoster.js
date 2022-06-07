0643a901022f5eac3c061704aebd12999de71985
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-06-07 11:55:30 +0300

    final

--- src/components/MoviePoster/MoviePoster.js
+++ src/components/MoviePoster/MoviePoster.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import {urls} from "../../constants";
 import {Rating} from "react-simple-star-rating";
+import {useSelector} from "react-redux";
 
-const MoviePoster = ({
-                         movie: {
-                             original_title,
-                             overview,
-                             title,
-                             vote_average,
-                             vote_count,
-                             poster_path,
-                             release_date
-                         }
-                     }) => {
+const MoviePoster = ({movie: {genre_ids,original_language,original_title, overview,title,vote_average,vote_count,poster_path,release_date}}) => {
+
+    const {genreList} = useSelector(state => state.genres)
+
+    const genresIdsConnection = genre_ids.map(value => genreList.find(genre => genre.id === value));
+    const genresNameIdsConnection = genresIdsConnection.map(value =>value.name);
+    const genresOfMovie = genresNameIdsConnection.toString().replaceAll(',', ', ')
 
     return (
         <div style={{display: 'flex'}}>
             <div><img style={{width: '600px', margin: '20px'}} src={`${urls.imageOriginal}/${poster_path}`}
                       alt={title}/></div>
             <div>
-                <div><h1>title: {title}</h1></div>
+                <div><h1>Title: {title}</h1></div>
                 <hr/>
-                <div><h3>Original title: {original_title}</h3></div>
+                <div><b>Original title: </b>{original_title} <br/><br/></div>
+                <div><b>Original language: </b>{original_language}<br/><br/></div>
+                <div><b>Genres: </b>{genresOfMovie}</div>
                 <div><h4>Overview:</h4> {overview}</div>
                 <div>Release date: {release_date}</div>
                 <div>
