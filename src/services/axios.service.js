e14dd86404b00b5ad05ef005062c88cfcd5f0637
Halia <91005548+Haliaa@users.noreply.github.com>
2024-04-28 09:49:22 +0300

    wip

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {baseURL} from "../constants";
 
-const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2N2QxNWM5ODBjODY4NmU0MGM2YTg2ZTdiM2I0ZDE0ZSIsInN1YiI6IjYwYzNjNmQzYTc2YWM1MDA1ODAwOTkwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OebMhuJQINmFUs-UFAxdpo_zGh7ZJ_3ZSCW7myYYTEA'
+const token = process.env.API_KEY
 const axiosService = axios.create({
     baseURL,
     headers: {
